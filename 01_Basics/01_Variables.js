2df4530eb2e52be0c9ce90dad750a6597fca7431
AVISHEK MISTRY <126349667+avishek195@users.noreply.github.com>
2024-05-22 03:05:18 +0000

    variables and datatypes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -2,8 +2,13 @@ const accountId = 9564452 // Change is not allowed
 let accountEmail = "avishekmistry420@gmail.com" // Change is allowed
 var accountPassword = "1234" // Change is allowed
 accountCity = "Thakurnagar" // Change is allowed
+let acccountSate; // It shows undefined beacuse no value assign in the variable
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
 
 
-
-
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, acccountSate]);
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //Treat all JS code as newer version or newer JS
+
+// alert() //this is for browser
+
+let name = "Avishek";
+let age = 22; // The range o fa numbe is 2 to power 53 approx
+let isLoggeddIn = false;
+
+//Primitive data types
+
+//Number
+//BigInt
+//string => ""
+// boolean => true/ false
+//null => standalone value
+//undifined => 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
