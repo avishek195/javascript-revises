a4ce37b7937666424a3546fc2f003996a2a3d809
avishek195 <avishekmistry420@gmail.com>
2024-05-22 12:29:22 +0530

    datatype conversion

---
+++ 01_Basics/03_Datatype_conversion_operation.js
@@ -0,0 +1,119 @@
+//01 Number conversion | Other datatype => number
+
+//Example 1
+// let score = "33";
+// console.log(typeof score);
+//let valueInNumber = Number(score); // datatype conversion
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//Example 2
+// let anotherScore = "56abc"; // ins
+// console.log(typeof anotherScore); //String
+// let valueInNumber = Number(anotherScore); // datatype conversion
+// console.log(valueInNumber); // NaN (Special type)
+// console.log(typeof valueInNumber); // Number
+
+//Example 3
+// let anotherScore = null;
+// console.log(typeof anotherScore); //object
+// let valueInNumber = Number(anotherScore); // datatype conversion
+// console.log(valueInNumber); // 0
+// console.log(typeof valueInNumber); // Number
+
+//Example 4
+// let anotherScore = undefined;
+// console.log(typeof anotherScore); //undefined
+// let valueInNumber = Number(anotherScore); // datatype conversion
+// console.log(valueInNumber); // NaN
+// console.log(typeof valueInNumber); // Number
+
+//Example 5
+// let anotherScore = true;
+// console.log(typeof anotherScore); // boolean
+// let valueInNumber = Number(anotherScore); // datatype conversion
+// console.log(valueInNumber); // 1
+// console.log(typeof valueInNumber); // Number
+
+//Eaxmple 6
+// let anotherScore = true;
+// console.log(typeof anotherScore); // boolean
+// let valueInNumber = Number(anotherScore); // datatype conversion
+// console.log(valueInNumber); // 1
+// console.log(typeof valueInNumber); // Number
+
+//02 Stirng Conversion | Other datatype => String
+
+//Example 1
+
+// let stringVar = 13;
+// console.log(typeof stringVar); // number
+// let valueInString = String(stringVar); // datatype conversion
+// console.log(valueInString); // "13"
+// console.log(typeof valueInString); // string
+
+//Example 2
+
+// let stringVar = 13;
+// console.log(typeof stringVar); // number
+// let valueInString = String(stringVar); // datatype conversion
+// console.log(valueInString); // "13"
+// console.log(typeof valueInString); // string
+
+//Example 3
+
+// let stringVar = true;
+// console.log(typeof stringVar); // boolean
+// let valueInString = String(stringVar); // datatype conversion
+// console.log(valueInString); // "true"
+// console.log(typeof valueInString); // string
+
+//Example 4
+
+// let stringVar = null;
+// console.log(typeof stringVar); // object
+// let valueInString = String(stringVar); // datatype conversion
+// console.log(valueInString); // "null"
+// console.log(typeof valueInString); // string
+
+//Example 5
+
+// let stringVar = undefined;
+// console.log(typeof stringVar); // undefined
+// let valueInString = String(stringVar); // datatype conversion
+// console.log(valueInString); // "undefined"
+// console.log(typeof valueInString); // string
+
+//03 Boolean Conversion | Other datatype => Boolean
+
+//Example 1
+
+// let boolVar = 23; any number include "+" or "-" the output will same
+// console.log(typeof boolVar); // number
+// let valueInBool = Boolean(boolVar); // datatype conversion
+// console.log(valueInBool); // true
+// console.log(typeof valueInBool); // boolean
+
+//Example 2
+
+// let boolVar = 0;
+// console.log(typeof boolVar); // number
+// let valueInBool = Boolean(boolVar); // datatype conversion
+// console.log(valueInBool); // false
+// console.log(typeof valueInBool); // boolean
+
+//Example 3
+
+// let boolVar = null;
+// console.log(typeof boolVar); // object
+// let valueInBool = Boolean(boolVar); // datatype conversion
+// console.log(valueInBool); // false
+// console.log(typeof valueInBool); // boolean
+
+//Example 4
+
+// let boolVar = undefined;
+// console.log(typeof boolVar); // undefined
+// let valueInBool = Boolean(boolVar); // datatype conversion
+// console.log(valueInBool); // false
+// console.log(typeof valueInBool); // boolean
