1307f7117e209e833fa5691ff985169e4150c25f
AVISHEK MISTRY <126349667+avishek195@users.noreply.github.com>
2024-05-22 02:43:07 +0000

    Variable revises

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,9 @@
+const accountId = 9564452 // Change is not allowed
+let accountEmail = "avishekmistry420@gmail.com" // Change is allowed
+var accountPassword = "1234" // Change is allowed
+accountCity = "Thakurnagar" // Change is allowed
+
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
