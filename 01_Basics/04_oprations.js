f2494b67b88dfd38563f237d088271b6214dd925
avishek195 <avishekmistry420@gmail.com>
2024-05-22 19:20:32 +0530

    oprations

---
+++ 01_Basics/04_oprations.js
@@ -0,0 +1,26 @@
+// console.log("1" + 2); // 12
+// console.log(2 + "1"); // 21
+// console.log("1" + 2 + 2); // 122
+// console.log(2 + 2 + "1"); // 41
+
+//confusion code
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+// let x = 10;
+// console.log(++(++x)); //error - invalid expression
+
+// let x2 = 3n; // 3n -> BigInt(datatype)
+// const y2 = x2++;
+// console.log(typeof x2); // BigInt
+// console.log(typeof y2); //BigInt
+// console.log(x2); // 4n
+// console.log(y2); // 3n
+
+// let x = 3;
+// const y = x++;
+// x is 4; y is 3
+
+// let x = 3;
+// const y = ++x;
+// x is 4; y is 4
