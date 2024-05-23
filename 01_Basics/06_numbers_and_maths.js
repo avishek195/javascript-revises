14f95f9bb4ece11bbd8701170f71cb00ee25ae49
avishek195 <avishekmistry420@gmail.com>
2024-05-23 20:49:26 +0530

    numbers and maths

---
+++ 01_Basics/06_numbers_and_maths.js
@@ -0,0 +1,36 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Numbers
+
+// const balance = 100;
+// console.log(balance); // 100
+// const anotherbalance = new Number(200);
+// console.log(anotherbalance); // [Number : 200]
+// console.log(anotherbalance.toString()); // 200 and this is a string
+// console.log(anotherbalance.toFixed(2)); // 200.00
+
+// const otherNumber = 123.454;
+// console.log(otherNumber.toPrecision(3)); // 123 and this is string
+// console.log(otherNumber.toString().length); // 7 include "."
+// let num = otherNumber.toString().replace(".", "");
+// console.log(num.length); // 6
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Maths
+
+// console.log(Math.abs(-4)); // 4 . This is you can say mod value.
+// console.log(Math.round(4.66)); // 5
+// console.log(Math.ceil(4.66)); // 5
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.min(4, 5, 6, 7, 9, 0)); // 0
+// console.log(Math.max(2, 4, 5, 78, 6)); // 78
+
+// console.log(Math.random()); // Range between 0 to 1
+// console.log(Math.floor(Math.random() * 10 + 1)); // Range between 1 to 10
+
+//Any range
+
+// let min = 15;
+// let max = 35;
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
