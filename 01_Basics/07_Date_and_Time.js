84a636bcde63ce7503c00f209dd798038b282d80
avishek195 <avishekmistry420@gmail.com>
2024-05-24 10:39:48 +0530

    Date snd Time

---
+++ 01_Basics/07_Date_and_Time.js
@@ -0,0 +1,46 @@
+// Dates
+
+// let myDate = new Date();
+
+// console.log(myDate); // 2024-05-24T01:57:21.316Z
+// console.log(typeof myDate); // Object
+// console.log(myDate.toString()); // Fri May 24 2024 07:29:31 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleString()); // 24/5/2024, 7:41:30 am
+// console.log(myDate.toISOString());// 2024-05-24T02:12:54.424Z
+// console.log(myDate.toDateString()); //Fri May 24 2024
+
+//CreateDate
+
+// let createDate = new Date(2023, 0, 23);
+
+// console.log(createDate.toDateString());// Mon Jan 23 2023
+// console.log(createDate.toLocaleString());// 23/1/2023, 12:00:00 am
+
+// Another way to create Date
+// let createDate = new Date(2023, 0, 23, 6, 3);
+// console.log(createDate.toDateString()); // Mon Jan 23 2023
+// console.log(createDate.toLocaleString()); // 23/1/2023, 6:03:00 am
+
+// TimeStamp
+
+// let myTimestamp = Date.now();
+// console.log(Math.floor(myTimestamp / 1000)); // milli second
+
+// +++++++++++++++++
+// console.log(myDate.getFullYear()); //2024
+// console.log(myDate.getDate()); // 24
+// console.log(myDate.getDay()); // 5
+// console.log(myDate.getHours()); // 7
+// console.log(myDate.getMinutes()); // 46
+
+//Current Time
+// let hour = myDate.getHours();
+// let minit = myDate.getMinutes();
+// console.log(`${hour}:${minit}`);
+
+//Current Date
+// let year = myDate.getFullYear();
+// let month = myDate.getMonth() + 1; // month start from 0
+// let day = myDate.getDay();
+
+// console.log(`${day}/${month}/${year}`);
